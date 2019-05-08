12b22ad4100712cac7d2a344fda34c16d8840220
tarun7886 <tarun1995jain@gmail.com>
2019-05-08 10:25:58 +0530

    focus removed on click

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import './styles/main.scss'
 import Routes from './container/Routes'
 
 class App extends Component {
+  componentDidMount() {
+    // Let the document know when the mouse is being used,
+    // so accessibility styling can be removed.
+
+    document.body.addEventListener('mousedown', function() {
+      document.body.classList.add('using-mouse')
+    })
+
+    document.body.addEventListener('keydown', function() {
+      document.body.classList.remove('using-mouse')
+    })
+  }
   render() {
     return (
       <div>
